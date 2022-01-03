977aa6ed4d35f0a97bebf957ba0bf9735f22b647
TheBadams <bpeteradams@gmail.com>
2022-01-03 12:00:29 -0500

    Wrote a test, confirming that Card.Config returns a new Instance of the Card class

--- test/lib/Card.test.ts
+++ test/lib/Card.test.ts
@@ -4,4 +4,10 @@ describe('Card Class', () => {
 	test('Card Class Should Exist', () => {
 		expect(Card).toBeDefined();
 	});
+	describe('Card.Config method', () => {
+		const newCard = Card.Config()
+		test('Card.Config Should return a new instance of a card', () => {
+			expect(newCard).toBeInstanceOf(Card)
+		})
+	})
 });
