25ea5742cf5728b413f25d1a659f2a5a69fb58b1
TheBadams <bpeteradams@gmail.com>
2022-01-03 11:58:12 -0500

    set up jest for use with typescript, set up first test run

---
+++ src/lib/Card.ts
@@ -0,0 +1,9 @@
+export default class Card {
+	constructor() {
+
+	}
+	
+	public static Config(): Card {
+		return new Card()
+	}
+}
---
+++ test/lib/Card.test.ts
@@ -0,0 +1,7 @@
+import Card from '../../src/lib/Card';
+
+describe('Card Class', () => {
+	test('Card Class Should Exist', () => {
+		expect(Card).toBeDefined();
+	});
+});
