8193d527c4f6efaba32bec409d895d75d64d849c
TheBadams <bpeteradams@gmail.com>
2022-01-03 12:49:53 -0500

    Added a value to the Card class and a test to test Configuration functionality: class properties and initialization complete

--- src/lib/Card.ts
+++ src/lib/Card.ts
@@ -1,9 +1,112 @@
-export default class Card {
-	constructor() {
+type Suit = 'Hearts' | 'Spades' | 'Diamonds' | 'Clubs'
+export enum Suits {
+	HEARTS = 'Hearts',
+	SPADES = 'Spades',
+	DIAMONDS = 'Diamonds',
+	CLUBS = 'Clubs'
+}
 
+type Face = 'Ace' | 'King' | 'Queen' | 'Jack' | 10 | 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2;
+
+export enum Faces {
+	ACE = 'Ace',
+	KING = 'King',
+	QUEEN = 'Queen',
+	JACK = 'Jack',
+	TEN = 10,
+	NINE = 9,
+	EIGHT = 8,
+	SEVEN = 7,
+	SIX = 6,
+	FIVE = 5,
+	FOUR = 4,
+	THREE = 3,
+	TWO = 2
+}
+
+type Value = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13
+export enum Values {
+	ACE = 1,
+	TWO,
+	THREE,
+	FOUR,
+	FIVE,
+	SIX,
+	SEVEN,
+	EIGHT,
+	NINE,
+	TEN,
+	JACK,
+	QUEEN,
+	KING
+}
+export interface ICardConfig {
+	suit: Suit;
+	face: Face;
+}
+export default class Card {
+	private suit: Suit
+	private face: Face
+	private value: Value
+	constructor(suit: Suit, face: Face, value: Value) {
+		this.suit = suit,
+		this.face = face,
+		this.value = value
+	}
+
+	private static FACES = Faces
+	private static VALUES = Values
+	private static generateCardValue(face: Face): Value {
+		let value: Value
+		switch (face) {
+			case this.FACES.ACE:
+				value = this.VALUES.ACE;
+				break;
+			case this.FACES.TWO:
+				value = this.VALUES.TWO;
+				break
+			case this.FACES.THREE:
+				value = this.VALUES.THREE;
+				break
+			case this.FACES.FOUR:
+				value = this.VALUES.FOUR;
+				break
+			case this.FACES.FIVE:
+				value = this.VALUES.FIVE;
+				break
+			case this.FACES.SIX:
+				value = this.VALUES.SIX;
+				break
+			case this.FACES.SEVEN:
+				value = this.VALUES.SEVEN;
+				break
+			case this.FACES.EIGHT:
+				value = this.VALUES.EIGHT;
+				break
+			case this.FACES.NINE:
+				value = this.VALUES.NINE;
+				break
+			case this.FACES.TEN:
+				value = this.VALUES.TEN;
+				break
+			case this.FACES.JACK:
+				value = this.VALUES.JACK;
+				break
+			case this.FACES.QUEEN:
+				value = this.VALUES.QUEEN;
+				break
+			case this.FACES.KING:
+				value = this.VALUES.KING;
+				break
+			default:
+				value = this.VALUES.TWO
+				break;
+		}
+		return value
 	}
 	
-	public static Config(): Card {
-		return new Card()
+	public static Config(cardConfig: ICardConfig): Card {
+		const value = this.generateCardValue(cardConfig.face)
+		return new Card(cardConfig.suit, cardConfig.face, value);
 	}
 }
--- test/lib/Card.test.ts
+++ test/lib/Card.test.ts
@@ -1,13 +1,26 @@
-import Card from '../../src/lib/Card';
+import Card, { Faces, ICardConfig, Suits, Values } from '../../src/lib/Card';
 
 describe('Card Class', () => {
 	test('Card Class Should Exist', () => {
 		expect(Card).toBeDefined();
 	});
 	describe('Card.Config method', () => {
-		const newCard = Card.Config()
+		const cardConfig: ICardConfig = {
+			suit: Suits.SPADES,
+			face: Faces.TWO
+		}
+		const newCard = Card.Config(cardConfig)
 		test('Card.Config Should return a new instance of a card', () => {
 			expect(newCard).toBeInstanceOf(Card)
+		})
+		test('newCard Should have a property, suit, equal to Suits.SPADES', () => {
+			expect(newCard).toHaveProperty('suit', Suits.SPADES);
+		})
+		test('newCard Should have a property, face, equal to Faces.TWO', () => {
+			expect(newCard).toHaveProperty('face', Faces.TWO);
+		})
+		test('newCard Should have a property, value, equal to Values.TWO', () => {
+			expect(newCard).toHaveProperty('value', Values.TWO);
 		})
 	})
 });
